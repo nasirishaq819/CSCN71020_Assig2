540136cfa7486d92543dff122fa2c6f61c1a37d3
nasirishaq819 <saninasirishaq@gmail.com>
2024-10-20 17:38:52 -0400

    I Added some tests to make sure its working properly

--- UnitTest1/UnitTest1.cpp
+++ UnitTest1/UnitTest1.cpp
@@ -26,12 +26,12 @@ namespace UnitTest1
 		
 		TEST_METHOD(Test_Perimeter)
 		{
-			int lenght = 15;
-			int width = 10;
+			int length = 10;
+			int width = 20;
 
-			int expectedPerimeter = 50;
+			int expectedPerimeter = 60;
 
-			int result = getPerimeter(&lenght, &width);
+			int result = getPerimeter(&length, &width);
 
 			Assert::AreEqual(expectedPerimeter, result);
 
@@ -39,16 +39,64 @@ namespace UnitTest1
 
 		TEST_METHOD(Test_Area) {
 
-			int length = 15;
+			int length = 20;
 			int witdth = 10;
 
-			int expectedArea = 150;
+			int expectedArea = 200;
 
 			int result = getArea(&length, &witdth);
 			Assert::AreEqual(expectedArea, result);
 
 
 		}
+
+		TEST_METHOD(Test_Length_valid) {
+
+			int length = 0;
+			int input = 30;
+			setLength(input, &length);
+			Assert::AreEqual(30, length);
+
+		}
+
+		TEST_METHOD(Test_lenght_belowValid) {
+
+			int lenght = 10;
+			int input = -1;
+			setLength(input, &lenght);
+			Assert::AreEqual(10, lenght);
+		}
+
+		TEST_METHOD(TestLenght_AboveValid) {
+			int lenght = 10;
+			int input = 101;
+			setLength(input, &lenght);
+			Assert::AreEqual(10, lenght);
+		}
+		TEST_METHOD(Test_width_Valid) {
+			int width = 0;
+			int input = 30;
+			setWidth(input, &width);
+			Assert::AreEqual(30, width);
+		}
+		TEST_METHOD(Test_width_BelowValid) {
+			int width = 30;
+			int input = 0;
+			setWidth(input, &width);
+			Assert::AreEqual(30, width);
+		}
+		TEST_METHOD(Test_width_AboveValid) {
+			int width = 20;
+			int input = 102;
+			setWidth(input, &width);
+			Assert::AreEqual(20, width);
+		}
+
+
+
+
+
+
 	};
 
 }
